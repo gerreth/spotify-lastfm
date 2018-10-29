57888e85a49502b4313a6c03e618d5b7ecba3b9b
gerreth <gerreth@gmx.net>
2018-10-29 21:09:28 +0100

    Resolve merge conflicts

--- frontend/app/containers/Discovery/styled.js
+++ frontend/app/containers/Discovery/styled.js
@@ -1,26 +1,6 @@
 import styled from 'styled-components';
 
 export const BandWrapper = styled.div`
-<<<<<<< HEAD
   margin: 0 auto;
   width: 600px;
-=======
-  position: relative;
-
-  :before {
-    content: '';
-    display: block;
-    padding-top: 100%;
-  }
-
-  img {
-    bottom: 0;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-    ${'' /* height: 100%; */} width: 100%;
-    z-index: 1;
-  }
->>>>>>> b5b943e9c334e2d0460eaa2db758860e4c482839
 `;
